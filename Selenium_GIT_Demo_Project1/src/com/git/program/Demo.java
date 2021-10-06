abb10d7834a2185f900705fb2a64aad620a04213
Asus <Asus@192.168.1.35>
2021-10-06 23:09:58 +0530

    by k

--- Selenium_GIT_Demo_Project1/src/com/git/program/Demo.java
+++ Selenium_GIT_Demo_Project1/src/com/git/program/Demo.java
@@ -1,7 +1,13 @@
 package com.git.program;
 
-public class Demo {
-
+public class Demo 
+{
+    public void m1()
+    {
+    	System.out.println("This is M1 Method");
+    }
+	
+	
 	public static void main(String[] args) 
 	{
 	  System.out.println("-------HELLO GIT-------");
